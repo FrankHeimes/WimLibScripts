3a27868526bc84eb0ea8b373e4d0b9c018feab93
Dr. Frank Heimes <frank.heimes@live.de>
2017-03-16 18:56:06 +0100

    Bug Fix: WIM backup file was not displayed

--- Backup.ps1
+++ Backup.ps1
@@ -1,4 +1,4 @@
-# Backup a volume, Version 1.0.17114
+# Backup a volume, Version 1.0.17114.1
 #
 # Original work Copyright (c) 2017 Dr. Frank Heimes (twitter.com/DrFGHde, www.facebook.com/dr.frank.heimes)
 #
@@ -58,109 +58,109 @@ $Global:imageName = (Get-Date).DateTime
 # Call wimlib-imagex with the specified arguments and throw on failure
 function ImageX([string[]]$wimlibArgs)
 {
-    if (!(Test-Path '.\wimlib-imagex.exe'))
-        { Throw "wimlib-imagex.exe not found. Please download from http://wimlib.net" }
+	if (!(Test-Path '.\wimlib-imagex.exe'))
+		{ Throw "wimlib-imagex.exe not found. Please download from http://wimlib.net" }
 
-    ''
-    $errorCount = $Error.Count
-    .\wimlib-imagex.exe $wimlibArgs
-    if (($LastExitCode -ne 0) -or ($Error.Count -gt $errorCount))
-        { Throw "wimlib-imagex.exe failed" }
+	''
+	$errorCount = $Error.Count
+	.\wimlib-imagex.exe $wimlibArgs
+	if (($LastExitCode -ne 0) -or ($Error.Count -gt $errorCount))
+		{ Throw "wimlib-imagex.exe failed" }
 }
 
 # Moves the window that executes this script to column 40 at the top of the screen
 function Move-WindowToTop
 {
-    $Global:Win32SetWindowPos = Add-Type –memberDefinition '[DllImport("user32.dll")] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);' `
-    -name 'Win32SetWindowPos' -namespace Win32Functions –passThru
-    [void]$Global:Win32SetWindowPos::SetWindowPos(((Get-Process -Id $pid).MainWindowHandle), 0, 40, 0, 0, 0, 0x4255)
+	$Global:Win32SetWindowPos = Add-Type –memberDefinition '[DllImport("user32.dll")] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);' `
+	-name 'Win32SetWindowPos' -namespace Win32Functions –passThru
+	[void]$Global:Win32SetWindowPos::SetWindowPos(((Get-Process -Id $pid).MainWindowHandle), 0, 40, 0, 0, 0, 0x4255)
 }
 
 # Make the host window almost the height of the screen and place it at the top
 function Configure-Host
 {
-    $MySize = $host.UI.RawUI.WindowSize
-    $MySize.Height = $host.UI.RawUI.MaxPhysicalWindowSize.Height - 2
-    $host.UI.RawUI.set_windowSize($MySize)
-    Move-WindowToTop
+	$MySize = $host.UI.RawUI.WindowSize
+	$MySize.Height = $host.UI.RawUI.MaxPhysicalWindowSize.Height - 2
+	$host.UI.RawUI.set_windowSize($MySize)
+	Move-WindowToTop
 }
 
 # Explain what the script does and let user confirm execution
 function Confirm-Execution
 {
-    "`n   B A C K U P   V O L U M E`n"
-    "This script will perform the following tasks:"
-    "------------------------------------------------"
-    " 1. Query the volume to backup."
-    " 2. Query the name of a WIM backup file."
-    " 3. Query a name for this backup."
-    " 4. Optionally make a copy of the WIM file if it exists."
-    " 5. Optionally call the Clean Manager to remove obsolete data."
-    "     You should run it once like this to configure what to delete:"
-    "       'cleanmgr /sageset:1'"
-    " 6. Create or update the backup file."
-    " 7. Verify the integrity of the backup file."
-    " 8. Create a log file with a list of files in the new image."
-    if ((Read-Host "`nContinue (Y/N)?") -ne 'y') { Exit }
+	"`n   B A C K U P   V O L U M E`n"
+	"This script will perform the following tasks:"
+	"------------------------------------------------"
+	" 1. Query the volume to backup."
+	" 2. Query the name of a WIM backup file."
+	" 3. Query a name for this backup."
+	" 4. Optionally make a copy of the WIM file if it exists."
+	" 5. Optionally call the Clean Manager to remove obsolete data."
+	"     You should run it once like this to configure what to delete:"
+	"       'cleanmgr /sageset:1'"
+	" 6. Create or update the backup file."
+	" 7. Verify the integrity of the backup file."
+	" 8. Create a log file with a list of files in the new image."
+	if ((Read-Host "`nContinue (Y/N)?") -ne 'y') { Exit }
 }
 
 # Let user select one of the available volumes to backup; CDRom volumes are skipped.
 function Select-Volume
 {
-    # Name = Caption = DeviceID, Optionally interesting: VolumeSerialNumber
-    Get-WmiObject Win32_logicaldisk | ?{ $_.DriveType -eq 3 } | Select Name,VolumeName,`
-    @{Name="Total[GB]"; Expression={[Math]::Ceiling($_.Size / 1GB)}},`
-    @{Name="Used[GB]"; Expression={[Math]::Ceiling(($_.Size - $_.FreeSpace) / 1GB)}},FileSystem,Description | `
-    Format-Table -AutoSize
-    "Enter the drive letter of the volume to backup."
-    $answer = Read-Host "This can be the live system, too ($($volume.TrimEnd(':\')))"
-    if (![string]::IsNullOrEmpty($answer)) { $Global:volume = $answer }
-    $Global:volume = $volume.ToUpper().TrimEnd(':\') + ':\'
+	# Name = Caption = DeviceID, Optionally interesting: VolumeSerialNumber
+	Get-WmiObject Win32_logicaldisk | ?{ $_.DriveType -eq 3 } | Select Name,VolumeName,`
+	@{Name="Total[GB]"; Expression={[Math]::Ceiling($_.Size / 1GB)}},`
+	@{Name="Used[GB]"; Expression={[Math]::Ceiling(($_.Size - $_.FreeSpace) / 1GB)}},FileSystem,Description | `
+	Format-Table -AutoSize
+	"Enter the drive letter of the volume to backup."
+	$answer = Read-Host "This can be the live system, too ($($volume.TrimEnd(':\')))"
+	if (![string]::IsNullOrEmpty($answer)) { $Global:volume = $answer }
+	$Global:volume = $volume.ToUpper().TrimEnd(':\') + ':\'
 }
 
 # Let user select or enter a WIM file. Aborts script if user hits 'Abort'
 function Get-WIMFilename
 {
-    $initialFolder = Split-Path -Parent $wimFile
-    "`nWIM files in ${initialFolder}:"
-    "--------------------------------"
-    if (Test-Path $initialFolder)
-        { Get-ChildItem -Path $initialFolder '*.wim' | Format-Table -Property FullName -AutoSize }
-    $answer = Read-Host "Enter the full path of an existing or new backup file ($wimFile)"
-    if (![string]::IsNullOrEmpty($answer)) { $Global:wimFile = $answer }
+	$initialFolder = Split-Path -Parent $wimFile
+	"`nWIM files in ${initialFolder}:"
+	"--------------------------------"
+	if (Test-Path $initialFolder)
+		{ Get-ChildItem -Path $initialFolder '*.wim' | Format-Table -Property FullName -AutoSize }
+	$answer = Read-Host "Enter the full path of an existing or new backup file ($wimFile)"
+	if (![string]::IsNullOrEmpty($answer)) { $Global:wimFile = $answer }
 }
 
 # Let user select or enter a WIM file. Aborts script if user hits 'Abort'
 function Get-WIMFilenameDlg
 {
-    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
-    $dialog = New-Object System.Windows.Forms.SaveFileDialog
-    $dialog.OverwritePrompt = $false
-    $dialog.initialDirectory = Split-Path -Parent $wimFile
-    $dialog.Title = "Select an existing backup file or select a folder and enter a new name"
-    $initialFilename = [IO.Path]::GetFileNameWithoutExtension($wimFile)
-    $dialog.FileName = $initialFilename
-    $dialog.filter = "WIM (*.wim)| *.wim"
-    [void]$dialog.ShowDialog()
-    if ($initialFilename -eq $dialog.FileName) { Exit }
-    $Global:wimFile = $dialog.FileName
+	[void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+	$dialog = New-Object System.Windows.Forms.SaveFileDialog
+	$dialog.OverwritePrompt = $false
+	$dialog.initialDirectory = Split-Path -Parent $wimFile
+	$dialog.Title = "Select an existing backup file or select a folder and enter a new name"
+	$initialFilename = [IO.Path]::GetFileNameWithoutExtension($wimFile)
+	$dialog.FileName = $initialFilename
+	$dialog.filter = "WIM (*.wim)| *.wim"
+	[void]$dialog.ShowDialog()
+	if ($initialFilename -eq $dialog.FileName) { Exit }
+	$Global:wimFile = $dialog.FileName
 }
 
 # Test if the WIM file is on the volume that is to be backed up, if yes, report this and exit script 
 function Reject-VolumeConflict
 {
-    if ($volume -eq "$(Split-Path -Qualifier $wimFile)\") {
-        Throw "The backup of volume '$volume' cannot be written to the WIM file '$wimFile' on that same volume."
-    }
+	if ($volume -eq "$(Split-Path -Qualifier $wimFile)\") {
+		Throw "The backup of volume '$volume' cannot be written to the WIM file '$wimFile' on that same volume."
+	}
 }
 
 # Let user override the name for this image
 function Get-ImageName
 {
-    "`nAll images in a WIM file must have unique names,"
-    "which should not contain special characters."
-    $answer = Read-Host "`nEnter a name for this backup image ($imageName)"
-    if (!([string]::IsNullOrEmpty($answer))) { $Global:imageName = $answer }
+	"`nAll images in a WIM file must have unique names,"
+	"which should not contain special characters."
+	$answer = Read-Host "`nEnter a name for this backup image ($imageName)"
+	if (!([string]::IsNullOrEmpty($answer))) { $Global:imageName = $answer }
 }
 
 # Query whether or not to run Windows Clean Manager
@@ -172,47 +172,47 @@ function Query-CleanMgr
 # Run Windows Clean Manager if configured
 function Run-CleanMgr
 {
-    if ($runCleanMgr)
-        { cleanmgr /sagerun:1 | Out-Null }  # | Out-Null waits for clean manager to finish
+	if ($runCleanMgr)
+		{ cleanmgr /sagerun:1 | Out-Null }  # | Out-Null waits for clean manager to finish
 }
 
 # Test if the specified file exists, if yes, report this and exit script 
 function Reject-ExistingBackup([string]$wimFileBackup) 
 {
-    if (Test-Path $wimFileBackup) {
-        Throw "File '$wimFileBackup' already exists.`n" + 
-        "Rename or delete it first and try again."
-    }
+	if (Test-Path $wimFileBackup) {
+		Throw "File '$wimFileBackup' already exists.`n" + 
+		"Rename or delete it first and try again."
+	}
 }
 
 # Query whether or not to create a backup of an existing WIM file
-function Query-WIMFileBackup
+function Query-WIMFileBackup([string]$wimFileBackup)
 {
-    if ($copyWIMFile)
+	if ($copyWIMFile)
 		{ $Global:copyWIMFile = (Read-Host "`nCopy '$wimFile' as '$wimFileBackup' (Y/N)?") -eq 'y' }
 }
 
 # Creates a copy of the WIM file after user confirmation
 function Create-WIMFileBackup([string]$wimFileBackup)
 {
-    if (!$copyWIMFile)
-        { return }
+	if (!$copyWIMFile)
+		{ return }
 
-    Reject-ExistingBackup $wimFileBackup
-    Import-Module BitsTransfer
-    Start-BitsTransfer -Source $wimFile -Destination $wimFileBackup `
-        -Description "Copying '$wimFile' as '$wimFileBackup'" -DisplayName "Copying backup file"
+	Reject-ExistingBackup $wimFileBackup
+	Import-Module BitsTransfer
+	Start-BitsTransfer -Source $wimFile -Destination $wimFileBackup `
+		-Description "Copying '$wimFile' as '$wimFileBackup'" -DisplayName "Copying backup file"
 }
 
 # Let user confirm all selected parameters before commencing operation. Abort script if user does not confirm.
-function Confirm-Parameters
+function Confirm-Parameters([string]$wimFileBackup)
 {
 	''
 	if ($runCleanMgr) { "The Windows Clean Manager will be called." }
-	if ($copyWIMFile) { "The existing WIM file '$wimFile' will be copied." }
-    "A backup of volume '$volume' with the name '$imageName'"
-    "will be added to the WIM file '$wimFile'"
-    if ((Read-Host "`nContinue (Y/N)?") -ne 'y') { Exit }
+	if ($copyWIMFile) { "The existing WIM file '$wimFile' will be copied as '$wimFileBackup'." }
+	"A backup of volume '$volume' with the name '$imageName'"
+	"will be added to the WIM file '$wimFile'"
+	if ((Read-Host "`nContinue (Y/N)?") -ne 'y') { Exit }
 }
 
 # $true if a new WIM file has been created, $false if an existing one was updated
@@ -221,48 +221,48 @@ $Global:newWIMFile = $false
 # Creates a named image of the specified volume in the specified WIM file
 function Create-Image
 {
-    if (Test-Path $wimFile) {
-	    "`nUpdating backup file '$wimFile' ..."
-        $command = 'append'
+	if (Test-Path $wimFile) {
+		"`nUpdating backup file '$wimFile' ..."
+		$command = 'append'
 		$Global:newWIMFile = $false
-    } else {
-	    "`nCreating new backup file '$wimFile' ..."
-        $command = 'capture'
+	} else {
+		"`nCreating new backup file '$wimFile' ..."
+		$command = 'capture'
 		$Global:newWIMFile = $true
-    }
-    ImageX $command, $volume, $wimFile, $imageName, --boot, --check, --solid, --snapshot
+	}
+	ImageX $command, $volume, $wimFile, $imageName, --boot, --check, --solid, --snapshot
 }
 
 # Verifies the integrity of the WIM file
 function Verify-WIMFile
 {
-    ImageX verify, $wimFile
+	ImageX verify, $wimFile
 }
 
 # Get the index of the latest image from the backup file. Abort script on failure
 function Get-ImageIndex
 {
-    if (ImageX info, $wimFile | ?{ $_ -match 'Image Count:\s*(\d+)' })
-        { return $Matches[1] }
+	if (ImageX info, $wimFile | ?{ $_ -match 'Image Count:\s*(\d+)' })
+		{ return $Matches[1] }
 
-    Throw "Could not extract the number of images in file '$wimFile'`n" +
-    "Please check the contents of the file yourself."
+	Throw "Could not extract the number of images in file '$wimFile'`n" +
+	"Please check the contents of the file yourself."
 }
 
 # Create a file list (i.e. table of contents) of the WIM file into a text file
 function Create-FileList
 {
-    $listFile = Join-Path (Split-Path -Parent $wimFile) $([IO.Path]::GetFileNameWithoutExtension($wimFile) + "$imageIndex.txt")
-    "`nListing files of image $imageIndex in '$wimFile' to '$listFile' ..."
-    ImageX dir, $wimFile, $imageIndex > $listFile
-    "Listed files of image $imageIndex into '$(Resolve-Path $listFile)'"
-    "You can add a description of the volume changes at the beginning of that file using a text editor."
+	$listFile = Join-Path (Split-Path -Parent $wimFile) $([IO.Path]::GetFileNameWithoutExtension($wimFile) + "$imageIndex.txt")
+	"`nListing files of image $imageIndex in '$wimFile' to '$listFile' ..."
+	ImageX dir, $wimFile, $imageIndex > $listFile
+	"Listed files of image $imageIndex into '$(Resolve-Path $listFile)'"
+	"You can add a description of the volume changes at the beginning of that file using a text editor."
 }
 
 # Compute and report the execution time w.r.t. the provided start time
 function Report-ExecutionTime([DateTime]$startTime)
 {
-    "`nBackup ran for " + ((Get-Date) - $startTime).ToString("h\:mm\:ss")
+	"`nBackup ran for " + ((Get-Date) - $startTime).ToString("h\:mm\:ss")
 }
 
 # Inform user that he can delete 
@@ -274,49 +274,49 @@ function Final-Message([string]$wimFileBackup)
 		"That includes booting the Windows on a USB drive or an installation CD."
 	}
 	
-    if (Test-Path $wimFileBackup) {
-        "`nYou may now move or delete the WIM backup file '$wimFileBackup'"
-        "if the updated WIM file '$wimFile' was verified successfully."
-    }
+	if (Test-Path $wimFileBackup) {
+		"`nYou may now move or delete the WIM backup file '$wimFileBackup'"
+		"if the updated WIM file '$wimFile' was verified successfully."
+	}
 }
 
 # Explain script, collect imputs from user and perform backup operation
 function Run-Backup
 {
-    Try {
-        if ($runInteractive) {
-            Configure-Host
-            Confirm-Execution
-            Select-Volume
-            if ($useFileDialogs) { Get-WIMFilenameDlg } else { Get-WIMFilename }
-            Reject-VolumeConflict
-            Get-ImageName
+	Try {
+		if ($runInteractive) {
+			Configure-Host
+			Confirm-Execution
+			Select-Volume
+			if ($useFileDialogs) { Get-WIMFilenameDlg } else { Get-WIMFilename }
+			Reject-VolumeConflict
+			Get-ImageName
 			Query-CleanMgr
 		}
+		$wimFileName = $([IO.Path]::GetFileNameWithoutExtension($wimFile))
+		$wimFileBackup = Join-Path (Split-Path $wimFile -Parent) "$wimFileName-$((Get-Date).ToShortDateString()).wim"
 		$Global:copyWIMFile = (Test-Path $wimFile)
-        if ($runInteractive) {
-			Query-WIMFileBackup
-            Confirm-Parameters
-        }
-        $wimFileName = $([IO.Path]::GetFileNameWithoutExtension($wimFile))
-        $wimFileBackup = Join-Path (Split-Path $wimFile -Parent) "$wimFileName-$((Get-Date).ToShortDateString()).wim"
-        Run-CleanMgr
-        Create-WIMFileBackup $wimFileBackup
-        $startTime = Get-Date
-        Create-Image
-        Verify-WIMFile
-        $Global:imageIndex = Get-ImageIndex
-        Create-FileList
-        Report-ExecutionTime $startTime
-        Final-Message $wimFileBackup
-        if ($runInteractive)
-            { Read-Host "`nDone. Press Enter to finish" }
-    }
-    Catch {
-        Write-Error $_
-        if ($runInteractive)
-            { Read-Host "`nPress Enter to abort" }
-    }
+		if ($runInteractive) {
+			Query-WIMFileBackup $wimFileBackup
+			Confirm-Parameters $wimFileBackup
+		}
+		Run-CleanMgr
+		Create-WIMFileBackup $wimFileBackup
+		$startTime = Get-Date
+		Create-Image
+		Verify-WIMFile
+		$Global:imageIndex = Get-ImageIndex
+		Create-FileList
+		Report-ExecutionTime $startTime
+		Final-Message $wimFileBackup
+		if ($runInteractive)
+			{ Read-Host "`nDone. Press Enter to finish" }
+	}
+	Catch {
+		Write-Error $_
+		if ($runInteractive)
+			{ Read-Host "`nPress Enter to abort" }
+	}
 }
 
 Set-Location ($MyInvocation.InvocationName | Split-Path -Parent)
